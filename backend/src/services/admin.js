66ea7af039580bb03d2da82a49551f675c00a997
monish <jrwebdev@micandmacsolutions.com>
2025-02-04 17:14:34 +0530

    jwt

--- backend/src/services/admin.js
+++ backend/src/services/admin.js
@@ -1,24 +1,26 @@
 const Admin = require("../models/admin");
 const { jwt_secerat } = require("../utils/constant");
+
+
 const checkAdminMobile = async (mobileNumber) => {
   const admin = await Admin.findOne({ mobile_no: mobileNumber });
 
-  try{
-  
+  try {
+
     if (admin) {
-        return admin
-     }
-     else{
-       throw new Error("No user found");
-     }
+      return admin
+    }
+    else {
+      throw new Error("No user found");
+    }
   }
-  catch(err){
+  catch (err) {
     throw new Error(err.message);
   }
-  
+
 };
 
 
-module.exports={
-    checkAdminMobile
+module.exports = {
+  checkAdminMobile
 }
