7fc1b0a265fa821f57bfe1e75f573015520766aa
monish <jrwebdev@micandmacsolutions.com>
2025-02-04 18:10:55 +0530

    Removed unnecessary files

--- src/utils/constant.js
+++
@@ -1,7 +0,0 @@
-// Load environment variables from .env file
-require('dotenv').config();
-
-// Export the JWT_SECRET environment variable using module.exports
-module.exports = {
-  jwt_secret: process.env.JWT_SECRET
-};
